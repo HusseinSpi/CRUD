b71fb64bbd13d85c97435aef3c86ade993ddd8a7
Rwaa Assi <rwaaassi123@gmail.com>
2024-06-10 15:09:21 +0300

    home page

--- store/src/Pages/Home/Home.jsx
+++ store/src/Pages/Home/Home.jsx
@@ -1,3 +1,18 @@
+import { GetBookData } from "../../ap/GetBookData";
+import { Card } from "../../Components/card/Card";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const { booksData, loading } = GetBookData();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="books-container">
+      {booksData.map((book) => (
+        <Card key={book.id} book={book} />
+      ))}
+    </section>
+  );
 };
